ea3ee8d9aa0551574e7ee88fda08e15f03da7f26
David Villasmil <david.villasmil.work@gmail.com>
2021-09-06 22:45:01 +0100

    Breanking things apart

--- directory.php
+++ directory.php
@@ -5,7 +5,7 @@ echo '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
     <section name="directory" description="Serving Directory Configuration for this user">
       <domain name="' . $_POST['domain'] . '">
         <user id="' . $_POST['user'] . '">
-	  <params>
+	      <params>
             <param name="vm-storage-dir" value="/efs"/>
             <param name="vm-storage-dir-shared" value="true"/>
             <param name="password" value="xyzzy"/>
@@ -14,7 +14,7 @@ echo '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
             <param name="vm-alternate-greet-id" value="8661234567"/>
           </params>
         </user>
-      </include>
+      </domain>
     </section>
   </document>';
 
