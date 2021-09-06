5cf47236cc2440f4ee744f8ff5f6fc8eacfbaedf
David Villasmil <david.villasmil.work@gmail.com>
2021-09-06 23:13:39 +0100

    Store VM recordings on EFS

--- directory.php
+++ directory.php
@@ -5,8 +5,8 @@ echo '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
     <section name="directory" description="Serving Directory Configuration for this user">
       <domain name="' . $_POST['domain'] . '">
         <user id="' . $_POST['user'] . '">
-	      <params>
-            <param name="vm-storage-dir" value="/efs"/>
+	  <params>
+            <param name="vm-storage-dir" value="/efs/vm-recording/' . $_POST['user'] . '"/>
             <param name="vm-storage-dir-shared" value="true"/>
             <param name="password" value="xyzzy"/>
             <param name="vm-enabled" value="true"/>
