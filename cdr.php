0cb9d597d72f1e34ecf2a2cb1aa9af09381d73f2
David Villasmil <david.villasmil.work@gmail.com>
2023-01-27 01:22:50 +0100

    add route and rate table

--- cdr.php
+++ cdr.php
@@ -28,17 +28,19 @@
         $write_codec=urldecode($xml->variables->write_codec);
         $read_codec=urldecode($xml->variables->read_codec);
         $context=urldecode($xml->callflow->caller_profile->context);
+        $route_table=urldecode($xml->variables->route_table);
+        $rate_table=urldecode($xml->variables->rate_table);
 
-        $query = "insert ws_cdr values ( NULL, '$datetime_start', '$sip_call_id', '$sip_from_user', '$sip_from_display', '$sip_to_user', '$datetime_answer', '$duration', '$received_ip','$rtp_audio_in_mos', '$rtp_audio_in_packet_count', '$rtp_audio_in_skip_packet_count', '$datetime_end', '$hangup_cause', '$hangup_cause_q850', '$remote_media_ip', '$read_codec', '$local_public_ip', '$write_codec','$context', '$last_app', '$last_arg' );";
+        $query = "insert ws_cdr values ( NULL, '$datetime_start', '$sip_call_id', '$sip_from_user', '$sip_from_display', '$sip_to_user', '$datetime_answer', '$duration', '$received_ip','$rtp_audio_in_mos', '$rtp_audio_in_packet_count', '$rtp_audio_in_skip_packet_count', '$datetime_end', '$hangup_cause', '$hangup_cause_q850', '$remote_media_ip', '$read_codec', '$local_public_ip', '$write_codec','$context', '$route_table', '$rate_table', '$last_app', '$last_arg' );";
 
-        $writefile = fopen('/tmp/dump.txt',"w");
-        fwrite($writefile,$query);
+        //$writefile = fopen('/tmp/dump.txt',"w");
+        //fwrite($writefile,$query);
 
         //fwrite($writefile,"===================");
         //fwrite($writefile,$_POST['cdr']);
         $stm = $dbh->query($query);
-        fwrite($writefile,"Statement: " . $stm);
-        fclose($writefile);
+        //fwrite($writefile,"Statement: " . $stm);
+        // fclose($writefile);
 
         if ($stm) {
             http_response_code(200);
